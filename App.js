c11783a92f426ae4821b5d979c5d2c87775dd8b4
geetaanjalishah <93533877+geetaanjalishah@users.noreply.github.com>
2024-06-07 00:24:17 +0530

    07/06

--- App.js
+++ App.js
@@ -5,36 +5,41 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
 const { MONGOURI } = require("./config/keys");
 
+// MongoDB connection
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 mongoose.connection.on("connected", () => {
-  console.log("connected to mango");
+  console.log("connected to mongo");
 });
-
 mongoose.connection.on("error", (err) => {
   console.log("error connecting", err);
 });
 
+// Require models
 require("./models/user");
-require('./models/post')
+require('./models/post');
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static('client/build'))
-  const path = require('path')
-  app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
+// Start server
 app.listen(PORT, () => {
   console.log("server is running on", PORT);
 });
