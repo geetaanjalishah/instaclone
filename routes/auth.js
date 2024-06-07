d5d40ccf40cbbd757e73d9692dc567c091caaa52
geetaanjalishah <93533877+geetaanjalishah@users.noreply.github.com>
2024-06-07 23:34:35 +0530

    07/06

--- routes/auth.js
+++ routes/auth.js
@@ -58,19 +58,16 @@ router.post("/signin", (req, res) => {
         if (doMatch) {
           const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
           const { _id, name, email, followers, following, pic } = savedUser;
-          console.log("Login successful");
           res.json({ message: "Login successful", token, user: { _id, name, email, followers, following, pic } });
         } else {
-          console.log("Invalid email or password");
           return res.status(422).json({ error: "Invalid email or password" });
         }
       });
     })
     .catch((err) => {
-      console.log("Internal server error:", err);
+      console.log(err);
       return res.status(500).json({ error: "Internal server error" });
     });
 });
 
 module.exports = router;
-
