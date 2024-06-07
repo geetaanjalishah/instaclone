02bf7ccaa56a3d59653a12c0b05f606a1d76ce7e
geetaanjalishah <93533877+geetaanjalishah@users.noreply.github.com>
2024-06-07 16:33:32 +0530

    07/06

--- App.js
+++ App.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
-const { MONGOURI } = require("./config/keys");
-
+const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
+const { MONGOURI } = require("./config/keys");
 
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 mongoose.connection.on("connected", () => {
-  console.log("connected to mongo");
+  console.log("connected to mango");
 });
+
 mongoose.connection.on("error", (err) => {
   console.log("error connecting", err);
 });
 
 require("./models/user");
-require("./models/post");
+require('./models/post')
 
 app.use(express.json());
 app.use(cors());
@@ -29,9 +28,10 @@ app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static('client/build'));
+  const path = require('path');
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
